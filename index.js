2816c652ddaf2ccff30ef368444e6b9dc033816c
Jules Joseph <julesj93@gmail.com>
2022-12-10 18:27:45 -0500

    added remaining files and edited bugs

--- index.js
+++ index.js
@@ -1,112 +1,132 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer'); 
-// TODO: Create an array of questions for user input
-const questions = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'github',
-            message: 'what is the title of your Project?',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter the title of your project');
-                    return false; 
-                }
-            } 
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'How would you describe your Project',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('Please provide a description of your  Project');
-                    return false; 
-                }
-            }
-    
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is your email address?',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your email address!');
-                    return false; 
-                }
-            }
-    
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What are the installaitons instructions for project',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your installation details');
-                    return false; 
-                }
-            }
-    
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'How do we use your application',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('Please tells how we use this application');
-                    return false; 
-                }
-            }
-    
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is your email address?',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your email address!');
-                    return false; 
-                }
-            }
-    
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is your github address?',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your github address');
-                    return false; 
-                }
-            }
-    
-        },
-    
+// Required: Inquirer Package | https://www.npmjs.com/package/inquirer/v/8.2.4 //
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
-// TODO: Create a function to write README file
-const writeToFile= require('generateMarkdown.js')
+// Array of questions for user input, info then put into generated README.md file //
+const questions = [
+    {
+        // For title input //
+        type: "input",
+        name: "title",
+        message: "Please enter the title of your project/application. (Required)",
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log ("Please enter a title to continue.")
+                return false;
+            }
+        }
+    },
+    {
+        // For description input //
+        type: "input",
+        name: "description",
+        message: "Please enter a description about your project/application. (Required)",
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+            console.log ("Please enter a description to continue.")
+            return false;
+            }
+        }
+    },
+    {
+        // For installation input //
+        type: "input",
+        name: "installation",
+        message: "Please enter any installation instructions if needed.",
+    },
+    {
+        // For usage input //
+        type: "input",
+        name: "usage",
+        message: "How is the application used?",
+    },
+    {
+        // For contribution guidelines input //
+        type: "input",
+        name: "contribution",
+        message: "What are the contribution guidelines?",
+    },
+    {
+        // For test instructions input //
+        type: "input",
+        name: "test",
+        message: "What are the test instructions for the project/application?",
+    },
+    {
+        // For license input; 'MIT' is set as the default choice //
+        type: "list",
+        name: "license",
+        message: "What is the license for the project/application?",
+        choices: ["MIT", "Apache 2.0", "Eclipse PL", "GNU GPL v3.0", "Unlicense", "None"],
+        default: "MIT"
+    },
+    {
+        // For GitHub username input //
+        type: "input",
+        name: "userName",
+        message: "What is your GitHub username for contact in case of any questions?",
+        validate: usernameInput => {
+            if (usernameInput) {
+                return true;
+            } else {
+            console.log ("Please enter your GitHub username to continue.")
+            return false;
+            }
+        }
+    },
+    {
+        // For email input //
+        type: "input",
+        name: "email",
+        message: "What is your email address for contact in case of any questions?",
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+            console.log ("Please enter an email address to continue.")
+            return false;
+            }
+        }
+    },
+    {
+        // For contribution guidelines input //
+        type: "input",
+        name: "credits",
+        message: "Enter the name(s) credited for this project/application.",
+    },
+];
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to write README file //
+const writeToFile = data => {
+    return new Promise((resolve,reject) => {
+        fs.writeFile("./genREADME.md", data, err => {
+            // if there is a error //
+            if (err) {
+                reject(err);
+                return;
+            }
+            // if there are no issues, README has been created successfully //
+            resolve({
+                ok: true,
+                message: console.log ("Your README file has been successfully created!")
+            });
+        });
+    });
+};
 
-// Function call to initialize app
+// Function to initialize app // 
+function init() {
+    inquirer.prompt(questions)
+    .then(function(data) {
+        writeToFile(generateMarkdown(data));
+    })
+}
+
+// Function call to initialize app //
 init();
